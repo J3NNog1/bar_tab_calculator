7ee871e898fb684b657aa3291717a5181e83852f
Jenn <jenniferjuarezjj@gmail.com>
2022-11-01 07:24:49 -0400

    mini project complete

--- bar_tab_calculator.py
+++ bar_tab_calculator.py
@@ -18,3 +18,13 @@ class Tab:
   def add(self, item):
     self.items.append(item)
     self.total += self.menu[item]
+
+  def print_bill(self, tax, service):
+    tax = (tax/100) * self.total
+    service = (service/100) * self.total
+    total = self.total + tax + service 
+
+    for item in self.items:
+      print(f'{item} ${self.menu[item]}')
+
+    print(f'{"Total"} ${total:.2f}')
